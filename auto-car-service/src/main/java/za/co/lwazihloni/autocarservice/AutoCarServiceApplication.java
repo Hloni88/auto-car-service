d2a1204d62d9ea35bd2642ee19d9bbd5ea1a6134
Hloni Mahlangu <hloni.mahlangu@momentum.co.za>
2020-05-12 23:26:32 +0200

    my first commit

---
+++ auto-car-service/src/main/java/za/co/lwazihloni/autocarservice/AutoCarServiceApplication.java
@@ -0,0 +1,13 @@
+package za.co.lwazihloni.autocarservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoCarServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoCarServiceApplication.class, args);
+	}
+
+}
