b86a198100826f323f3dc81d528d954ea0ca532c
Hloni Mahlangu <hloni.mahlangu@momentum.co.za>
2020-05-19 19:48:06 +0200

    enabling swagger and swagger-ui

--- auto-car-service/src/main/java/za/co/app/autocarservice/model/SalesPerson.java
+++ auto-car-service/src/main/java/za/co/app/autocarservice/model/SalesPerson.java
@@ -1,4 +1,31 @@
 package za.co.app.autocarservice.model;
 
-public class SalesPerson {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+
+@Entity
+@Table(name ="sales_person")
+@Getter
+@Setter @NoArgsConstructor
+public class SalesPerson implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id ;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "first_name")
+    private String firstName;
+
+    public SalesPerson(Long id, String lastName, String firstName) {
+        this.id = id;
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
 }
