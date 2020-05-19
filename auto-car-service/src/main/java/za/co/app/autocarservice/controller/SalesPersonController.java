dc755686c1411e7184c4743e3d8af2b8c6667a6b
Hloni Mahlangu <hloni.mahlangu@momentum.co.za>
2020-05-19 19:16:43 +0200

    enabling swagger and swagger-ui

--- auto-car-service/src/main/java/za/co/app/autocarservice/controller/SalesPersonController.java
+++ auto-car-service/src/main/java/za/co/app/autocarservice/controller/SalesPersonController.java
@@ -1,4 +1,54 @@
 package za.co.app.autocarservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import za.co.app.autocarservice.model.SalesPerson;
+import za.co.app.autocarservice.service.SalesPersonService;
+
+
+import java.util.List;
+
+
+
+@RestController
+@RequestMapping("/api")
 public class SalesPersonController {
+
+    @Autowired
+    SalesPersonService salesPersonService;
+
+
+
+    @PostMapping("/salesPersons")
+    public ResponseEntity<SalesPerson> createSalesPerson(@RequestBody SalesPerson salesPerson) {
+        return ResponseEntity.ok().build();
+
+    }
+
+    @PutMapping("/salesPersons/{id}")
+    public ResponseEntity<SalesPerson> updateSalesPerson(@RequestBody SalesPerson salesPerson, @PathVariable long id) {
+        return ResponseEntity.noContent().build();
+    }
+
+
+    @GetMapping(value = "/salesPersons")
+    public List<SalesPerson> getAllSalesPersons() {
+        List<SalesPerson> salesPersonList = salesPersonService.retrieveAllSalesPersons();
+        return salesPersonList ;
+
+    }
+
+    @GetMapping(value = "/salesPersons/{id}")
+    public SalesPerson getSalesPersonById(@PathVariable long id) {
+
+        return null;
+    }
+
+    @DeleteMapping("/salesPersons/{id}")
+    public void deleteSalesPerson(@PathVariable long id) {
+
+    }
+
+
 }
