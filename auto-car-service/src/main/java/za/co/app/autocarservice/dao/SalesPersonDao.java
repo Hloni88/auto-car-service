d8a3f10b6584f0578728fcbdf1bd3babc30e3eb9
Hloni Mahlangu <hloni.mahlangu@momentum.co.za>
2020-05-19 19:16:03 +0200

    enabling swagger and swagger-ui

--- auto-car-service/src/main/java/za/co/app/autocarservice/dao/SalesPersonDao.java
+++ auto-car-service/src/main/java/za/co/app/autocarservice/dao/SalesPersonDao.java
@@ -1,10 +1,19 @@
 package za.co.app.autocarservice.dao;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import za.co.app.autocarservice.model.SalesPerson;
 
+import java.util.List;
+
+
 @Repository
-public interface SalesPersonDao extends CrudRepository<SalesPerson,Long> {
+public interface SalesPersonDao extends JpaRepository<SalesPerson,Long> {
+     List<SalesPerson> findByLastName(String lastName);
+
+    @Query("SELECT p FROM SalesPerson p ")
+    List<SalesPerson> find();
+
 
 }
